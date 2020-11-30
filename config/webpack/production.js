8e9d6f99052eb66491682cd2c4ec728a0ab58915
b3nty <benty1988@hotmail.com>
2020-11-30 18:42:27 +0100

    heroku push

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.includePaths = ['node_modules']
-
 module.exports = environment.toWebpackConfig()

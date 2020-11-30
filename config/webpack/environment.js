40a34140defd14294852e7a034dd6007da370659
b3nty <benty1988@hotmail.com>
2020-11-30 17:57:25 +0100

    heroku push

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,4 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
-// Get the actual sass-loader config
-const sassLoader = environment.loaders.get('sass')
-const sassLoaderConfig = sassLoader.use.find(function(element) {
-  return element.loader == 'sass-loader'
-})
-
-// Use Dart-implementation of Sass (default is node-sass)
-const options = sassLoaderConfig.options
-options.implementation = require('sass')
-
 module.exports = environment
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.includePaths = ['node_modules']
+
 module.exports = environment.toWebpackConfig()
